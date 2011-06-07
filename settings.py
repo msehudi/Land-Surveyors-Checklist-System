5ea62097c5f33473f177e4ac21c4b254e0515753
Grigory <grigory.kruglov@gmail.com>
2011-06-07 14:15:34 -0700

    Remove staticfiles app; add local_settings import

--- settings.py
+++ settings.py
@@ -5,6 +5,7 @@ TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
     ('Ryan', 'wallacer@uvic.ca'),
+	('Grigory', 'grigory.kruglov@gmail.com'),
 )
 
 MANAGERS = ADMINS
@@ -115,11 +116,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
+
     'south',
     'checklist',
 )
@@ -146,3 +145,8 @@ LOGGING = {
         },
     }
 }
+
+try:
+	from local_settings import *
+except ImportError:
+	pass
