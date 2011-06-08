4c18c9879330513a2b680da2ea6056fe74b763bd
Ryan Wallace <wallacer@uvic.ca>
2011-06-08 16:00:08 -0700

    added some simple CSS

--- checklist/urls.py
+++ checklist/urls.py
@@ -9,5 +9,8 @@ urlpatterns = patterns('',
 	url(r'^inbox', inbox, name='inbox'),
 	#url(r'^assignment', showAssignment, name='assignment'),
 	url(r'^assignment/(?P<assignmentID>\d+)/$', showAssignment, name='assignment'),
+	
+	# can probably leave out the checklistID since assignments can currently only have 1 checklist
+	#  Unless...  we want an assignment to be able to consist of more than 1 checklist.
 	url(r'^assignment/(?P<assignmentID>\d+)/checklist/(?P<checklistID>\d+)/$', showChecklist, name='checklist'),
 )
--- urls.py
+++ urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import redirect_to
+import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -9,7 +11,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'surveyor.views.home', name='home'),
     # url(r'^surveyor/', include('surveyor.foo.urls')),
 
-	
+	url(r'^$', redirect_to, {'url' : '/checklist/login/'}),
 	url(r'^checklist/', include('checklist.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
@@ -17,4 +19,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+	(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+	    {'document_root': settings.MEDIA_ROOT}),
 )
